9b7677ed8eb4825f491686aec03ea13d5ae3c3db
Godruoyi <godruoyi@gmail.com>
2024-12-06 15:24:43 +0800

    clean image optimize by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
@@ -13,7 +13,7 @@ export default defineConfig({
     image: {
         // If you don't want to optimize images during the BUILD process,
         // you can open this comment. It will significantly reduce the build time but won't optimize any images anymore.
-        // service: passthroughImageService(),
+        service: import.meta.env.ASTRO_IMAGE_OPTIMIZE ? undefined : passthroughImageService(),
     },
     integrations: [
         partytown(),
