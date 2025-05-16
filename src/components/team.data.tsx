0c2ed7c2050b40aa8523c7c456f4a2023194e555
Saeed Abadiyan <sd.eed1381@gmail.com>
2025-05-16 21:48:33 +0330

    update teams

--- src/components/team.data.tsx
+++ src/components/team.data.tsx
@@ -27,7 +27,8 @@ const teams =
   { university: 'شوکت', name: 'دیوکد' },
   { university: 'شوکت', name: ' God of PY' },
   { university: 'بقیه دانشگاه ها', name: 'Ctrl+C' },
-  { university: 'شوکت', name: 'future programmers' }
+  { university: 'شوکت', name: 'future programmers' },
+  { university: 'شوکت', name: 'Faisa' }
 ];
 
 
@@ -56,13 +57,19 @@ export default function FinalTeamsTable() {
           Final Teams for the BCPC Contest
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm sm:text-base max-w-sm mb-8">
-            Team not found? Please <a className="underline font-bold" href="/contact">contact us</a>
+          Team not found? Please <a className="underline font-bold" href="/contact">contact us</a>
         </p>
 
         <div className="overflow-x-auto rounded-lg border border-neutral-300 dark:border-neutral-700">
           <table className="min-w-full divide-y divide-neutral-300 dark:divide-neutral-700">
             <thead className="bg-neutral-200 dark:bg-neutral-900">
               <tr>
+                <th
+                  scope="col"
+                  className="px-4 py-3 text-left text-lg sm:text-xl font-semibold text-neutral-700 dark:text-neutral-300"
+                >
+                  #
+                </th>
                 <th
                   scope="col"
                   className="px-4 py-3 text-left text-lg sm:text-xl font-semibold text-neutral-700 dark:text-neutral-300"
@@ -80,10 +87,13 @@ export default function FinalTeamsTable() {
             <tbody className="bg-white dark:bg-neutral-800 divide-y divide-neutral-300 dark:divide-neutral-700">
               {teams.map(({ name, university }, i) => (
                 <tr key={i} className={i % 2 === 0 ? "bg-white dark:bg-neutral-800" : "bg-neutral-50 dark:bg-neutral-900"}>
-                  <td className="p-2 py-2 sm:px-4  text-neutral-900 dark:text-neutral-100 whitespace-nowrap text-sm sm:text-base">
+                  <td className="px-2 py-4 sm:px-4 text-neutral-900 dark:text-neutral-100 whitespace-nowrap text-sm sm:text-base">
+                    {i + 1}
+                  </td>
+                  <td className="p-2 py-2 sm:px-4 text-neutral-900 dark:text-neutral-100 whitespace-nowrap text-sm sm:text-base">
                     {name}
                   </td>
-                  <td className="px-2 py-4 sm:px-4  text-neutral-900 dark:text-neutral-100 whitespace-nowrap text-sm sm:text-base">
+                  <td className="px-2 py-4 sm:px-4 text-neutral-900 dark:text-neutral-100 whitespace-nowrap text-sm sm:text-base">
                     {switchUni(university)}
                   </td>
                 </tr>
